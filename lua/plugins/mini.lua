4201edb7bef6bd85fda590577834af20128f1722
Geraldo Lopes de Souza <geraldo.ls@gmail.com>
2025-11-03 20:14:04 -0300

    desliguei around do mini pq estava interferindo com () no clojure

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,7 +9,9 @@ return {
     --  - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
     --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup { n_lines = 500 }
+    --
+    -- Desliguei porque estava bagunçando no clojure a seleção de parenteses
+    -- require('mini.ai').setup { n_lines = 500 }
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
